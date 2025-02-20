14d1f93c3c696831d4e12ee1a8b1f6507c6ab670
Zeev <dumalaganalen22@gmail.com>
2025-02-20 22:52:54 +0800

    Hero changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Hero from './components/Hero.jsx';
 
+
 function App() {
   return (
     <div className="app-container">
       <Routes>
         <Route path="/" element={<Hero />} />
-       
+     
       </Routes>
     </div>
   );
